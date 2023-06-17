be895ecd0d0963e5be17e8d307d04ac9a711296c
robert <phuonghoanganh@admicro.vn>
2023-06-17 10:28:23 +0700

    first commit

---
+++ src/main/java/vn/unigap/java/ServiceDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package vn.unigap.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryServerApplication.class, args);
+	}
+
+}
